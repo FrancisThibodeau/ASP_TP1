58f66a855e2b75dc8095429bdf36710fd40b6715
Frank Rioux <francoisrioux67@hotmail.com>
2015-03-19 12:20:43 -0400

    Profil - Update fonctionnel

--- TP1/Profil.aspx.cs
+++ TP1/Profil.aspx.cs
@@ -9,9 +9,13 @@ namespace TP1
 {
     public partial class Profil : System.Web.UI.Page
     {
+        TableUsers user = null;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             ((Label)Master.FindControl("LBL_Titre")).Text = "Modifier votre profil";
+
+            user = (TableUsers)Session["User"];
 
             if (!Page.IsPostBack)
             {
@@ -21,13 +25,16 @@ namespace TP1
 
         protected void BTN_Update_Click(object sender, EventArgs e)
         {
-
+            if (Page.IsValid)
+            {
+                UpdateUser();
+                Session["User"] = user;
+                Response.Redirect("Index.aspx");
+            }
         }
 
         private void LoadUserInfo()
         {
-            TableUsers user = (TableUsers)Session["User"];
-
             TB_Fullname.Text = user.Fullname;
             TB_Username.Text = user.Username;
             TB_Password.Text = TB_PasswordConfirm.Text = user.Password;
@@ -35,6 +42,27 @@ namespace TP1
 
             if (user.Avatar != "")
                 IMG_PreviewAvatar.ImageUrl = @"~\Avatars\" + user.Avatar + ".png";
+        }
+
+        private void UpdateUser()
+        {
+            user.Fullname = TB_Fullname.Text;
+            user.Username = TB_Username.Text;
+            user.Password = TB_Password.Text;
+            user.Email = TB_Email.Text;
+
+            String avatar_ID;
+            if (FU_Avatar.FileName != "")
+            {
+                String Avatar_Path = "";
+
+                avatar_ID = Guid.NewGuid().ToString();
+                Avatar_Path = Server.MapPath(@"~\Avatars\") + avatar_ID + ".png";
+                FU_Avatar.SaveAs(Avatar_Path);
+                user.Avatar = avatar_ID;
+            }
+
+            user.Update();
         }
 
         protected void CV_Fullname_ServerValidate(object source, ServerValidateEventArgs args)
@@ -57,12 +85,6 @@ namespace TP1
             if (TB_Username.Text == "")
             {
                 TB_Username.BackColor = System.Drawing.Color.FromArgb(0, 255, 200, 200);
-                args.IsValid = false;
-            }
-            else if (user.SelectByFieldName("USERNAME", TB_Username.Text))
-            {
-                TB_Username.BackColor = System.Drawing.Color.FromArgb(0, 255, 200, 200);
-                CV_Username.ErrorMessage = "Ce nom d'usager est déjà pris";
                 args.IsValid = false;
             }
             else
