5db92bdd8339ac7e29de997918203dd631256a8d
Frank Rioux <francoisrioux67@hotmail.com>
2015-04-01 09:25:38 -0400

    Update BD -TableThread

---
+++ TP1/TableThreads.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP1
+{
+    public class TableThreads : SqlExpressUtilities.SqlExpressWrapper
+    {
+        public long ID { get; set; }
+        public long Creator { get; set; }
+        public String Title { get; set; }
+        public DateTime DateCreation { get; set; }
+
+        public TableThreads(String connexionString, System.Web.UI.Page page)
+            : base(connexionString,page)
+        {
+            SQLTableName = "THREADS";
+        }
+
+        public override void GetValues()
+        {
+            ID = long.Parse(this["ID"]);
+            Creator = long.Parse(this["CREATOR"]);
+            Title = this["TITLE"];
+            DateCreation = DateTime.Parse(this["DATE_CREATION"]);
+        }
+
+        public override void Insert()
+        {
+            InsertRecord(Creator, Title, DateCreation);
+        }
+
+        public override void Update()
+        {
+            UpdateRecord(ID, Creator, Title, DateCreation);
+        }
+    }
+}
