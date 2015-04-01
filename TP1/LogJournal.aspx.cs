c743be03c05131a704589fb7acfd478a11aaa9e8
Francis Thibodeau <francis.thbd@gmail.com>
2015-04-01 13:57:06 -0400

    weird stuff une metode a disparu d'un wrapper

--- TP1/LogJournal.aspx.cs
+++ TP1/LogJournal.aspx.cs
@@ -9,6 +9,8 @@ namespace TP1
 {
     public partial class LogJournal : System.Web.UI.Page
     {
+        public bool Admin = false;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             ((Label)Master.FindControl("LBL_Titre")).Text = "Room";
@@ -18,19 +20,23 @@ namespace TP1
         private void AfficherListe()
         {
             TableUsers table = new TableUsers((String)Application["MainDB"], this);
+            if (Session["UserName"].ToString() == "admin")
+                Admin = true;
+
             table.SelectAll();
-            //table.MakeGridView(PN_GridView, "");
+            table.MakeGridView(PN_GridView, "");
             table.EndQuerySQL();
         }
 
-       // public virtual bool SelectAll(string orderBy = "")
-       // {
-       //     string sql = "SELECT * FROM " + SQLTableName;
-       //     if (orderBy != "")
-       //         sql += " ORDER BY " + orderBy;
-       //     QuerySQL(sql);
-       //     return reader.HasRows;
-       // }
+       public override bool SelectAll(string orderBy = "")
+       {
+           string sql = "SELECT Logins.ID,UserId,LoginDate,LogoutDate,IPAddress,FullName,UserName,Email,Avatar FROM " + SQLTableName + " inner join Users on Users.ID = Logins.UserID where UserName = '" + UserName + "'";
+
+           if (orderBy != "")
+               sql += " ORDER BY " + orderBy;
+           QuerySQL(sql);
+           return reader.HasRows;
+       }
 
         protected void BTN_Retour_Click(object sender, EventArgs e)
         {
