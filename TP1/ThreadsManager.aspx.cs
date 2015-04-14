398302634bd5f13a31427f8e22aec0a4c78ced46
Francis Thibodeau <francis.thbd@gmail.com>
2015-04-14 18:37:16 -0400

    Ajout d'un item dans les list

--- TP1/ThreadsManager.aspx.cs
+++ TP1/ThreadsManager.aspx.cs
@@ -14,24 +14,38 @@ namespace TP1
             ((Label)Master.FindControl("LBL_Titre")).Text = "Gestion des discussions";
         }
 
-        protected void CVal_TitreDiscussion_ServerValidate(object source, ServerValidateEventArgs args)
-        {
-        
-        }
-
-        protected void CVal_DiscussionExiste_Exists(object source, ServerValidateEventArgs args)
-        {
-            
-        }
-
-        protected void CBOX_AllUsers_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
+        //protected void CVal_TitreDiscussion_ServerValidate(object source, ServerValidateEventArgs args)
+        //{
+        //
+        //}
+        //
+        //protected void CVal_DiscussionExiste_Exists(object source, ServerValidateEventArgs args)
+        //{
+        //    
+        //}
+        //
+        //protected void CBOX_AllUsers_CheckedChanged(object sender, EventArgs e)
+        //{
+        //
+        //}
 
         protected void BTN_New_Click(object sender, EventArgs e)
         {
+            if (TBX_NewThread.Text != null)
+            {
+                ListItem newItem = new ListItem(TBX_NewThread.Text, "0");
+                LBL_ListDiscussions.Items.Add(newItem);
+                TBX_NewThread.Text = null;
 
+                CBX_Users.Items.Add(newItem);
+
+                // call de update panel
+            }
+            else
+            {
+                TBX_NewThread.Text = null;
+                // call de update panel
+            }
         }
 
         protected void BTN_Modify_Click(object sender, EventArgs e)
